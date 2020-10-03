f456f2e7fa1b1ea7fe88d43ecb08f3edee540404
Bartosz Majewski <majewskibartosz@live.com>
2020-10-03 11:20:29 +0200

    Add styled components to Error components

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Error = () => <h1>Oops! Page not found!</h1>
+const Container = styled.div`
+  width: 100%;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+`
+const Text = styled.h1`
+  width: 100%;
+  color: white;
+  text-align: center;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 350%;
+  font-weight: 500;
+  line-height: normal;
+  user-select: none;
+  text-shadow: 2px 3px 4px rgba(0, 0, 0, 0.3);
+
+  position: fixed;
+  top: 37%;
+  left: 50%;
+  transform: translate(-50%, 60%);
+`
+
+const Error = () => (
+  <Container>
+    <Text>Oops! Page not found!</Text>
+  </Container>
+)
 
 export default Error
