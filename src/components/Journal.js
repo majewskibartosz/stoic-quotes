2446b426d887e43d88ea0fee0c30ea0691a6e303
Bartosz Majewski <majewskibartosz@live.com>
2020-11-07 09:05:56 +0100

    moving new repo

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import EdiText from 'react-editext'
 
 const Container = styled.div`
   width: 100%;
@@ -24,9 +25,24 @@ const Text = styled.h1`
   transform: translate(-50%, 60%);
 `
 
+const Example = (props) => {
+  const [value, setValue] = useState('What is real? How do you define real?')
+
+  const handleSave = (val) => {
+    console.log('Edited Value -> ', val)
+    setValue(val)
+  }
+  return (
+    <div className="container">
+      <EdiText type="text" value={value} onSave={handleSave} />
+    </div>
+  )
+}
+
 const Journal = () => (
   <Container>
     <Text>This is Journal.</Text>
+    <Example></Example>
   </Container>
 )
 
