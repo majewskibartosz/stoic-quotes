8bf6e887fdb99768c198e3f09070d70bd27f6389
Bartosz Majewski <majewskibartosz@live.com>
2020-12-14 22:19:22 +0100

    few changes to make it production repo

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ import reset from 'styled-reset'
 import MainScreen from './MainScreen'
 import Background from './Background'
 import Overlay from './Overlay'
-import Journal from './Journal'
+// import Journal from './Journal'
 import Error from './Error'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
@@ -18,14 +18,14 @@ const GlobalStyle = createGlobalStyle`
 const App = () => (
   <>
     <GlobalStyle />
-    <Navbar />
+    {/* <Navbar /> */}
     <Overlay />
     <Background />
     <Switch>
       <Route path="/" component={MainScreen} exact />
-      <Route path="/journal">
-        <Journal />
-      </Route>
+      {/* <Route path="/journal"> */}
+      {/* <Journal /> */}
+      {/* </Route> */}
       <Route component={Error} />
     </Switch>
   </>
